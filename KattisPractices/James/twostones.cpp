f2055152dd46f35fee545dbdd8c2303e65b9cc27
aaryamNUS <aaryamps1@gmail.com>
2019-03-25 18:39:50 +0800

    Removed some files

--- KattisPractices/James/twostones.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main () {
-	int x;
-	cin >> x;
-	(x%2)? cout << "Alice\n" : cout << "Bob\n";
-	return 0;
-}
--- KattisPractices/wilson/house_of_cards.py
+++
@@ -1,9 +0,0 @@
-def numCards(num):  
-    return (3*pow(num, 2) + num) // 2 # Floor division
-
-
-height = int(input())  
-while (numCards(height) % 4 != 0):  
-    height += 1
-
-print(height)  
